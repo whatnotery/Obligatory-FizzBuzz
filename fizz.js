cdfd6404dd8530234f3fe74da53883ff6948e0fe
Jos O'shea <jos.oshea.ky@gmail.com>
2021-09-21 23:15:36 -0400

    initial commit

---
+++ fizz.js
@@ -0,0 +1,21 @@
+let maxNum = prompt('enter a max number')
+for (i = 1; i <= maxNum; i++) {
+        
+    if (i % 15 === 0) {
+        addDiv('fizzbuzz')
+    } else if (i % 5 === 0) {
+        addDiv('buzz')
+    } else if (i % 3 === 0) {
+        addDiv('fizz')
+    } else {
+        addDiv(i)
+    }
+
+}
+
+
+function addDiv(i) {
+    let div = document.createElement('div')
+    div.innerText = `> ${i}`;
+    return document.body.appendChild(div)
+} 
