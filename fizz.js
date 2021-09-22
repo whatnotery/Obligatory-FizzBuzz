4cb84c75585cccf0ca0fa887a1954f6520ec8237
John O'shea <johncyoshea@me.com>
2021-09-22 11:43:33 -0400

    refactoring

--- fizz.js
+++ fizz.js
@@ -1,30 +1,38 @@
-input = document.querySelector('#number');
+let input = document.querySelector('#number');
+let container = document.querySelector('#container');
+let button = document.querySelector('#button')
+
+
 
 input.addEventListener('keypress', function (e) {
-      let maxNum = parseInt(input.value)
-      if (e.key === 'Enter') {
-
-      for (i = 1; i <= maxNum; i++) {
-        
-        if (i % 15 === 0) {
-            addDiv('fizzbuzz')
-        } else if (i % 5 === 0) {
-            addDiv('buzz')
-        } else if (i % 3 === 0) {
-            addDiv('fizz')
-        } else {
-            addDiv(i)
+    if (e.key === 'Enter') {
+        let maxNum = parseInt(input.value)
+        for (i = 1; i <= maxNum; i++) {
+            if (i % 15 === 0) {
+                addDiv('fizzbuzz')
+            } else if (i % 5 === 0) {
+                addDiv('buzz')
+            } else if (i % 3 === 0) {
+                addDiv('fizz')
+            } else {
+                addDiv(i)
+            }
         }
-    
-    }}
+    }
 });
 
 
-
+button.addEventListener('click', function (e) {
+    clear()
+})
 
 
 function addDiv(i) {
     let div = document.createElement('div')
     div.innerText = `> ${i}`;
-    return document.body.appendChild(div)
-} 
+    return container.appendChild(div).className = 'num'
+}
+
+function clear() {
+    document.querySelectorAll('.num').forEach(e => e.remove());
+}
