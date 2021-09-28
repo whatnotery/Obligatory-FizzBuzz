1a9ca5a4e1a9c0ee9997c78db04006b966841223
John O'shea <johncyoshea@me.com>
2021-09-28 16:41:46 -0400

    improvements

--- fizz.js
+++ fizz.js
@@ -1,12 +1,12 @@
-let input = document.querySelector('#number');
-let container = document.querySelector('#container');
+const input = document.querySelector('#number');
+const container = document.querySelector('#container');
 
 input.focus()
 
-input.addEventListener('keypress', function (e) {
-
+input.addEventListener('keydown', function (e) {
     if (e.key === 'Enter') {
-        let maxNum = parseInt(input.value)
+        if (input.value){
+        let maxNum = input.value
         for (i = 1; i <= maxNum; i++) {
             if (i % 15 === 0) {
                 addDiv('fizzbuzz')
@@ -21,31 +21,32 @@ input.addEventListener('keypress', function (e) {
         addButton()
 
         input.value = ''
-        input.blur()
+        input.disabled = true;
 
         let button = document.querySelector('button')
 
         button.addEventListener('click', function (e) {
             clear()
         })
-    }
+    }}
 });
 
 function addDiv(i) {
     let div = document.createElement('div')
-    div.innerText = `> ${i}`;
+    div.innerHTML  = `<i class="bi bi-arrow-right-short"></i>
+    ${i}`;
     return container.appendChild(div).className = 'num'
 }
 
 function clear() {
     document.querySelectorAll('.num').forEach(e => e.remove());
-
-
+    input.disabled = false;
+    input.focus()
 }
 
 function addButton() {
     let createButton = document.createElement('button')
     createButton.setAttribute('class', 'num')
-    createButton.innerText = 'clear'
+    createButton.innerText = 'Clear To Continue'
     container.appendChild(createButton)
 }
