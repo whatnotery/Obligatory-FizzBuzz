fa0ae91b210e7788e164991ed361e677b91f4444
John O'shea <johncyoshea@me.com>
2021-09-28 17:32:07 -0400

    refactoring

--- fizz.js
+++ fizz.js
@@ -1,52 +1,54 @@
 const input = document.querySelector('#number');
-const container = document.querySelector('#container');
+const container = document.querySelector('ul');
+const body = document.querySelector('body')
+let prevRun = false
 
 input.focus()
 
+body.addEventListener('click', function () {
+    input.focus()
+})
+
+
 input.addEventListener('keydown', function (e) {
     if (e.key === 'Enter') {
-        if (input.value){
-        let maxNum = input.value
-        for (i = 1; i <= maxNum; i++) {
-            if (i % 15 === 0) {
-                addDiv('fizzbuzz')
-            } else if (i % 5 === 0) {
-                addDiv('buzz')
-            } else if (i % 3 === 0) {
-                addDiv('fizz')
-            } else {
-                addDiv(i)
-            }
-        }
-        addButton()
-
-        input.value = ''
-        input.disabled = true;
-
-        let button = document.querySelector('button')
-
-        button.addEventListener('click', function (e) {
+        if (input.value && prevRun === false) {
+            let maxNum = input.value
+            fizzBuzz(maxNum)
+            prevRun = true
+            input.value = ''
+        } else if (input.value && prevRun === true) {
             clear()
-        })
-    }}
+            let maxNum = input.value
+            fizzBuzz(maxNum)
+            input.value = ''
+        }
+    }
 });
 
 function addDiv(i) {
-    let div = document.createElement('div')
-    div.innerHTML  = `<i class="bi bi-arrow-right-short"></i>
-    ${i}`;
-    return container.appendChild(div).className = 'num'
+    let li = document.createElement('li')
+    li.innerHTML = `${i}`;
+    return container.appendChild(li).className = 'num'
 }
 
 function clear() {
     document.querySelectorAll('.num').forEach(e => e.remove());
-    input.disabled = false;
+    let prevRun = false
     input.focus()
+
 }
 
-function addButton() {
-    let createButton = document.createElement('button')
-    createButton.setAttribute('class', 'num')
-    createButton.innerText = 'Clear To Continue'
-    container.appendChild(createButton)
+function fizzBuzz(maxNum) {
+    for (i = 1; i <= maxNum; i++) {
+        if (i % 15 === 0) {
+            addDiv('fizzbuzz')
+        } else if (i % 5 === 0) {
+            addDiv('buzz')
+        } else if (i % 3 === 0) {
+            addDiv('fizz')
+        } else {
+            addDiv(i)
+        }
+    }
 }
